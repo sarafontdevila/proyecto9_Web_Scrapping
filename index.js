09c2d04a55d77fa942c84589d7087d79dbb9c3cf
sarafontdevila <146539364+sarafontdevila@users.noreply.github.com>
2025-04-06 16:33:58 +0200

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+const { scrapper } = require('./src/scrapperFlamenc/scrapper')
+const express = require ("express")
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const funcionElemento = async (req, res, next) => {
+  try {
+    const { elemento } = req.params
+    const imgs = await scrapper(`https://trajesflamencos.eu/${elemento}`)
+  return res.status(200).json(imgs)
+  } catch (error) {
+    return res.status(400).json("no encontradas")
+  }
+  }
+
+app.use("/api/v1/:elemento", funcionElemento)
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000/");
+
+})
+
+
+scrapper('https://trajesflamencos.eu/41-vestidos-adulto')
---
+++ src/scrapperFlamenc/scrapper.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer')
+
+const scrapper = async (url) => {
+
+  const arrayImgs = []
+  const browser = await puppeteer.launch({ headless: false})
+  const page = await browser.newPage()
+  await page.goto(url)
+
+  const divsImg = await page.$$('a.product_img_link')
+  console.log(divsImg.length)
+
+  /*for (let i=0; i < 10; i++){ 
+    const div = divsImg[i];
+    const img = await div.$eval("img", (e) => e.src);
+    console.log(img);*/
+    
+  for (let i = 0; i < Math.min(10,divsImg.length); i++) {
+    const div = divsImg[i]
+    try {
+      const img = await div.$eval('img', (e) => e.src)
+      arrayImgs.push(img)
+
+    } catch (error) {
+      console.log('No image found in this container ${i}')
+    }
+    
+  }
+  console.log(arrayImgs)
+  browser.close()
+  return arrayImgs
+}
+
+module.exports = { scrapper }
